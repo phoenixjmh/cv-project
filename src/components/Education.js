14c6e67b375d36fb1ad018c16d7389f265f94d81
phoenixjmh <phoenixjmh@gmail.com>
2023-05-02 11:20:35 -0600

    Add button implemented for Work, and education fields, Education field now displays the added module above the form. Next, need to do the same for the work field

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,30 +3,109 @@ import React, { Component } from 'react';
 class Education extends Component {
     constructor(props) {
         super(props)
+        this.displayForm=this.displayForm.bind(this);
+        this.appendModule=this.appendModule.bind(this);
+        this.handleChange=this.handleChange.bind(this);
+        this.state={
+            eduModules:0,
+            currName:null,
+            currStudy:null,
+            currStudyDate:null,
+            index:0,
+            modules:[]
+            
+
+            
+        }
     }
-    render() {
-        return (
-            <fieldset>
+
+    handleChange(e,idx){
+        if(e.target.id==='school-name')
+            this.setState({currName:e.target.value})
+
+        
+        if(e.target.id==='area-of-study')
+            this.setState({currStudy:e.target.value})
+
+        if(e.target.id==='date-of-study')
+            this.setState({currStudyDate:e.target.value})
+
+        
+        // console.log(this.state);
+       
+    }
+
+    appendModule(e){
+        e.preventDefault();
+        this.setState({index:this.state.index+1})
+        this.setState({
+            modules:this.state.modules.concat({
+                name:this.state.currName,
+                study:this.state.currStudy,
+                date:this.state.currStudyDate,
+                index:this.state.index
+            })
+        })
+        this.setState({
+            currName:null,
+            currStudy:null,
+            currStudyDate:null,
+            
+        })
+        console.log(this.state.index);
+        
+    }
+    
+    displayForm(idx){
+        return(
+        <fieldset>
                 <legend> Educational Experience</legend>
                 <ul>
                     <li>
-                <label htmlFor="school-name"><span className='form-label'>School Name</span>
-                    <input type='text' id='school-name'name = 'school'required ></input>
+                <label htmlFor={"school-name"+idx}><span className='form-label'>School Name</span>
+                    <input type='text' id='school-name'name = 'school' onChange={(event)=>this.handleChange(event,idx)}required ></input>
                 </label>
 
                     </li>
                     <li>
                     <label htmlFor="area-of-study"><span className='form-label'>Area of study</span>
-                    <input type='text' id='area-of-study'name = 'study'required ></input>
+                    <input type='text' id='area-of-study'name = 'study' onChange={(event)=>this.handleChange(event,idx)} required ></input>
                 </label>
                     </li>
                     <li>
                     <label htmlFor="date-of-study"><span className='form-label'>Date studied</span>
-                    <input type='date' id='date-of-study'name = 'study-date'required ></input>
+                    <input type='date' id='date-of-study'name = 'study-date' onChange={(event)=>this.handleChange(event,idx)} required ></input>
                     </label>
                     </li>
                 </ul>
+                <button id='append-education-experience'onClick={this.appendModule}>Append to CV</button>
             </fieldset>
+        )
+    }
+    render() {
+        return (
+            <>
+            {this.state.modules.length>0? this.state.modules.map(item=>{
+               return (
+                   <div className='education-module'id = {'edu-module'+item.index} key={'edu-module'+item.index}>
+                    {console.log(item.index)}
+               <p>{item.name}</p>
+               <p>{item.study}</p>
+               <p>{item.date}</p>
+               </div>
+               )
+            }):0}
+            {this.state.eduModules>0? this.displayForm(this.state.eduModules):<></>}
+            <button id='add-school' onClick={(e)=>{
+                e.preventDefault();
+                this.setState({
+                    eduModules:this.state.eduModules+1,
+                    idx:0
+                    
+                });
+                console.log(this.state.eduModules);
+            }}>Add</button>
+            </>
         )
     }
 }
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,8 +2,74 @@ import React, {Component} from 'react';
 class Experience extends Component{
 constructor(props){
     super(props)
+    this.displayForm=this.displayForm.bind(this);
+    this.appendModule=this.appendModule.bind(this);
+    this.handleChange=this.handleChange.bind(this);
+    this.state={
+        expModules:0,
+        currName:null,
+        currPosition:null,
+        currFromDate:null,
+        currToDate:null,
+        modules:[]
+        
+
+        
+    }
 }
-render(){
+handleChange(e,idx){
+    if(e.target.id==='company-name')
+        this.setState({currName:e.target.value})
+
+    
+    if(e.target.id==='position')
+        this.setState({currPosition:e.target.value})
+
+    if(e.target.id==='date-from')
+        this.setState({currFromDate:e.target.value})
+
+        if(e.target.id==='date-to')
+        this.setState({currToDate:e.target.value})
+
+
+    
+    // console.log(this.state);
+   
+}
+appendModule(e){
+    e.preventDefault();
+    this.setState({
+        modules:this.state.modules.concat({
+            name:this.state.currName,
+            position:this.state.currPosition,
+            datefrom:this.state.currFromDate,
+            dateto:this.state.currToDate
+        })
+    })
+    this.setState({
+        currName:null,
+        currStudy:null,
+        currStudyDate:null
+    })
+    console.log(this.state.modules);
+    
+}
+
+render() {
+    return (
+        <>
+        {this.state.expModules>0? this.displayForm(this.state.expModules):<></>}
+        <button id='add-exp' onClick={(e)=>{
+            e.preventDefault();
+            this.setState({
+                expModules:this.state.expModules+1
+            });
+            console.log(this.state.expModules);
+        }}>Add</button>
+        </>
+    )
+}
+displayForm(){
     return(
         <fieldset>
             <legend>Work Experience</legend>
@@ -11,23 +77,29 @@ render(){
 
             <li>
                 <label htmlFor = "company-name"><span>Company name</span>
-                <input type = 'text' id = 'company-name' name='company' required ></input>
+                <input type = 'text' id = 'company-name' name='company' 
+                onChange={this.handleChange} required ></input>
                 </label>
             </li>
             <li>
             <label htmlFor = "position"><span>Position Title</span>
-                <input type = 'text' id = 'position' name='position' required ></input>
+                <input type = 'text' id = 'position' name='position'
+                onChange={this.handleChange} required ></input>
                 </label>
             </li>
 
             <li>
             <label htmlFor = "date-from"><span>Time worked</span>
-                <input type = 'date' id = 'date-from' name= 'from-date'required ></input> required 
-                <input type ='date' id = 'date-to' name='to-date'></input>
+                <input type = 'date' id = 'date-from' name= 'from-date'
+                onChange={this.handleChange}required ></input> required 
+                <input type ='date' id = 'date-to' name='to-date'
+                onChange={this.handleChange}></input>
                 </label>
 
             </li>
         </ul>
+        <button id='append-work-experience'onClick={this.appendModule}>Append to CV</button>
+
         </fieldset>
 
     )
