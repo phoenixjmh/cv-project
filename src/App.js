6e5e1a43bf4917e35ab76d595f54a9c8dd50dd91
phoenixjmh <phoenixjmh@gmail.com>
2023-05-12 15:50:42 -0600

    Main app component was converted to hook structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import PersonalInfo from "./components/PersonalInfo";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
@@ -6,32 +6,27 @@ import PreviewPane from "./components/PreviewPane";
 import "./styles/App.scss";
 import "./styles/PreviewPanel.scss";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isPreview: false,
-      cvApp: null,
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleErrors = this.handleErrors.bind(this);
-  }
-  handleSubmit(e) {
+const App = () => {
+  const [isPreview, setIsPreview] = useState(false);
+  const [cvApp, setCVApp] = useState(null);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     let form = e.target;
-    if (this.handleErrors(form)) {
+    if (handleErrors(form)) {
       let formData = new FormData(form);
 
       let dataJSON = Object.fromEntries(formData);
-      this.setState({
-        cvApp: dataJSON,
-        isPreview: true,
-      });
+
+      setCVApp(dataJSON);
+      setIsPreview(true);
+    
     } else {
       return;
     }
-  }
-  handleErrors(form) {
+  };
+
+  const handleErrors = (form) => {
     const expForm = form.childNodes[1].childNodes[1];
     const eduForm = form.childNodes[2].childNodes[1];
     if (expForm.className === "modular-form-section") {
@@ -44,38 +39,34 @@ class App extends Component {
     }
 
     return true;
-  }
+  };
 
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <EditPane />
-          <button type="submit" onClick={() => this.handleSubmit}>
-            Preview
-          </button>
-        </form>
-        {this.state.isPreview ? (
-          <PreviewPane
-            cv={this.state.cvApp} //UNCOMMENT FOR BUILD
-            callback={() => this.setState({ isPreview: false })}
-          />
-        ) : null}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <EditPane />
+        <button type="submit" onClick={() => handleSubmit}>
+          Preview
+        </button>
+      </form>
+      {isPreview ? (
+        <PreviewPane
+          cv={cvApp} //UNCOMMENT FOR BUILD
+          callback={() => setIsPreview(false)}
+        />
+      ) : null}
+    </>
+  );
+};
 
-class EditPane extends Component {
-  render() {
-    return (
-      <>
-        <PersonalInfo />
-        <Experience />
-        <Education />
-      </>
-    );
-  }
-}
+const EditPane = () => {
+  return (
+    <>
+      <PersonalInfo />
+      <Experience />
+      <Education />
+    </>
+  );
+};
 
 export default App;
